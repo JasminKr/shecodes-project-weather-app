205029ffb389588bcada65f86e7e4a853d1de40d
JasminKr <jasmin.kraeutner@gmail.com>
2021-01-29 16:45:25 +0100

    Showing current temperature

--- SheCodesPlus_Project/src/index.js
+++ SheCodesPlus_Project/src/index.js
@@ -1,1 +1,25 @@
 
+
+function displayTemperature(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    //descriptionElement.innerHTML = (response.data.weather[0].main);
+}
+
+
+
+let apiKey = "ec1d278a516090bf3bd43a3271490af9";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+
+
+//18.06
