0a9d324a4d3660686145897bc0155c2813a8fc3a
JasminKr <jasmin.kraeutner@gmail.com>
2021-01-29 17:46:12 +0100

    added icon from API

--- SheCodesPlus_Project/src/index.js
+++ SheCodesPlus_Project/src/index.js
@@ -20,17 +20,26 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     descriptionElement.innerHTML = (response.data.weather[0].description);
     dateElement.innerHTML = formateDate(response.data.dt * 1000);
-}
+    iconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+        );
+    iconElement.setAttribute(
+            "alt",
+            response.data.weather[0].description);
+    
+    }
 
 
-
+let city = "Lisbon"
 let apiKey = "ec1d278a516090bf3bd43a3271490af9";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
